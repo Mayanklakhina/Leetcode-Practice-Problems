b911532434b308c373dc0ef9883e3c5e42c9d9ff
Mayanklakhina <90982737+Mayanklakhina@users.noreply.github.com>
2022-05-08 13:33:29 +0530

    Time: 19 ms (20.70%), Space: 11.5 MB (50.16%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int max = 0;
+        int n=arr.size();
         
-        for(int i = 1;i<arr.size();i++){
-            if(arr[i]>arr[max])
-                max=i;
+        //firstly initialize a variable peak and assume the peak index value as 0
+        int peak = 0;
+        
+        for(int i = 1;i<n;i++){
+    //now we are checking that if the element present at i is greater than element present at index peak so we will update the value of peak index.
+            if(arr[i]>arr[peak])
+                peak=i;
         }
-        return  max;
+        return  peak;
     }
 };
