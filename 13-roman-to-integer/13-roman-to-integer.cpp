86c8c58d81671127deab54d47f8434eaf0d063e9
Mayanklakhina <90982737+Mayanklakhina@users.noreply.github.com>
2022-05-11 00:23:32 +0530

    Time: 41 ms (6.54%), Space: 7.7 MB (47.24%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -17,7 +17,7 @@ public:
         for(int i=0;i<n;i++){
             
             //condition for let's say this roman numerals IV = 4 (5-1)
-            if(hash[s[i]]<hash[s[i+1]])
+            if(i<n-1&&hash[s[i]]<hash[s[i+1]])
                 
                 sum-=hash[s[i]];    //so for IV here, sum would be -1
             else
