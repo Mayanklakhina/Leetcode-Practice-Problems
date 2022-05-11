1ee46de4c2b37cfa081229f4450522b8537b4ae1
Mayanklakhina <90982737+Mayanklakhina@users.noreply.github.com>
2022-05-11 19:45:35 +0530

    Time: 4 ms (95.88%), Space: 13.7 MB (19.29%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     
     vector<int> searchRange(vector<int>& nums, int target) {
-               vector<int>ans;
-        int i = 0, j = nums.size()-1;
-        int start = -1, end = -1;
-        while(i <= j)
-        {
-            int mid = (i+j)/2;
-            if(nums[mid] == target){
-               int temp = mid;
-                while(mid > 0 && nums[mid-1] == target) //iterating left side of mid until target value is found
-                    mid--;
-                start = mid;
-                while(temp < nums.size()-1 && nums[temp+1] == target) //iterating right side of mid until target value is found (here temp is equal to mid)
-                    temp++;
-                end = temp;
-                
-                ans.push_back(start);
-                ans.push_back(end);
-                return ans;
-            }
-            else if(nums[mid] > target)
-                j = mid-1;
-            else
-                i = mid+1;
-        }
+        vector<int> result;
+        int n = nums.size();
+        int low = 0;
+        int high=n-1;
+        int i,j;
+  
         
-        ans.push_back(-1);
-        ans.push_back(-1);
-        return ans;
-    
+        
+        while(low<=high){
+            int mid = (high-low)/2+low;
+            if(nums[mid]==target){
+                int temp = mid;
+                
+                while(mid>0 && nums[mid-1]==target)
+                    mid--;
+                  i = mid;
+                while(temp<nums.size()-1&&nums[temp+1]==target)
+                    temp++;
+                j=temp;
+             
+        result.push_back(i);
+        result.push_back(j);
+                return result;
+   
+            }
+            else if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        result.push_back(-1);
+        
+        result.push_back(-1);
+        return result;
     }
 };
