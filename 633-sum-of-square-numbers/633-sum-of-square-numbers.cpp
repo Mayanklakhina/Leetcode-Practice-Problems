816790f7d623cb36d8711395dbff42d7e7603e2c
Mayanklakhina <90982737+Mayanklakhina@users.noreply.github.com>
2022-05-16 12:27:45 +0530

    Time: 3 ms (68.59%), Space: 6 MB (26.44%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        long long int low = 0, high=sqrt(c);
+        //using two pointers technique
+        long long int low = 0, high=sqrt(c); 
+        
+        //here sqrt is a function that will return the square of a number 
         
         while(low<=high){
-            if(low*low+high*high==c){
+            if(low*low+high*high==c){ 
                 return true;
             }
             else if(low*low+high*high>c){
-                high--;
+                high--;                      
             }
             else{
                 low++;
