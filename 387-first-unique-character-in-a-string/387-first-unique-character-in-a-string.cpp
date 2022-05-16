34e1f7142c0fb3af9571f602aba958126f71a939
Mayanklakhina <90982737+Mayanklakhina@users.noreply.github.com>
2022-05-16 20:15:37 +0530

    Time: 60 ms (34.79%), Space: 10.7 MB (44.37%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+// declare an unordered map which will have a key as a character and value as integer value
         unordered_map<char,int> hash;
         
         for(int i=0;i<s.size();i++){
+            //traversing the string and storing the frequency of each character
             hash[s[i]]++;
         }
         for(int i=0;i<s.size();i++){
