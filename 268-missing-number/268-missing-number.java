6a68510d0a4f6419488d9c9629b814d8c847bb9a
Mayank <90982737+Mayanklakhina@users.noreply.github.com>
2022-05-28 16:01:56 +0530

    Time: 1 ms (65.97%), Space: 51.2 MB (36.97%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,22 +1,11 @@
 class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-            if(nums[nums.size()-1]==0){
-            return 1;
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int n = nums.length;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
         }
-            int n =nums.size(); ///its last number
-           for (int i=0;i< n-1;i++){
-//after sorting going thorugh every element with its preceding element if both are equal incerement
-            if((nums[i]+1)!=nums[i+1]) {
-                return (nums[i] + 1);
-            }
-                  
-             if(nums[n-1]+1==n) {
-                return n;
-             }
-        }    
-            return 0;
-  
+        return n*(n+1)/2 - sum ;
+
     }
-};
+}
