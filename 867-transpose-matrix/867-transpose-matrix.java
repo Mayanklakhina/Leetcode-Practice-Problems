eb21eb6a0ecec75e1882f4ed485262d64c8f6cc3
Mayank <90982737+Mayanklakhina@users.noreply.github.com>
2022-06-02 21:07:47 +0530

    Time: 1 ms (52.16%), Space: 48.4 MB (57.35%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.java
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -1,11 +1,10 @@
 class Solution {
-public:
-    vector<vector<int>> transpose(vector<vector<int>> A) {
-        int M = A.size(), N = A[0].size();
-        vector<vector<int>> B(N, vector<int>(M, 0));
+    public int[][] transpose(int[][] A) {
+        int M = A.length, N = A[0].length;
+        int[][] B = new int[N][M];
         for (int j = 0; j < N; j++)
             for (int i = 0; i < M; i++)
                 B[j][i] = A[i][j];
         return B;
     }
-};
+}
