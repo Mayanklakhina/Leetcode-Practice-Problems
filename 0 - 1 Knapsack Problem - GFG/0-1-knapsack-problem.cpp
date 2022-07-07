68aecb1a22ccb4dc5baa2f834db370351b024918
Mayank <90982737+Mayanklakhina@users.noreply.github.com>
2022-07-07 14:46:36 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,53 +8,32 @@ class Solution
 {
     public:
 //     //Function to return max value that can be put in knapsack of capacity W.
-//     int knapSack(int W, int wt[], int val[], int n,vector<vector<int>>&t) 
-//     { 
-//       //base condition
-//       if(n==0||W==0)
-//       return 0;
-//       if(t[n][W]!=-1)
-//       return t[n][W];
+    int knap(int W, int wt[], int val[], int n,vector<vector<int>>&t) 
+    { 
+      //base condition
+      if(n==0||W==0)
+      return 0;
+      if(t[n][W]!=-1)
+      return t[n][W];
        
-//       //choices we have
-//       if(wt[n-1]<=W){
-//           return t[n][W]=max(val[n-1]+knapSack(W-wt[n-1],wt,val,n-1,t),knapSack(W,wt,val,n-1,t));
-//       }
-//       else{
-//           return t[n][W]=knapSack(W,wt,val,n-1,t);
-//       }
-//     }
+      //choices we have
+      if(wt[n-1]<=W){
+          return t[n][W]=max(val[n-1]+knap(W-wt[n-1],wt,val,n-1,t),knap(W,wt,val,n-1,t));
+      }
+      else if(wt[n-1]>W){
+          return t[n][W]=knap(W,wt,val,n-1,t);
+      }
+    }
     
-//     int knap(int W, int wt[], int val[], int n) 
-//   { 
-//       // Your code here
-//       vector<vector<int>>t(n+1,vector<int>(W+1,-1));
-      
-//       return knapSack(W,wt,val,n,t);
-      
-//   }
-int knap(int W,int wt[],int val[],int n,vector<vector<int>>&dp){
-       if(W==0 or n==0)return 0;
-      if(dp[n][W]!=-1)return dp[n][W];
-      if(W>=wt[n-1]){
-          int inc=val[n-1]+knap(W-wt[n-1],wt,val,n-1,dp);
-          int exc=knap(W,wt,val,n-1,dp);
-          return dp[n][W]= max(inc,exc);
-      }
-      else if(W<wt[n-1]){
-          int exc=knap(W,wt,val,n-1,dp);
-          return dp[n][W]=exc;
-      }
-   }
-   
-   int knapSack(int W, int wt[], int val[], int n) 
-   { 
+    int knapSack(int W, int wt[], int val[], int n) 
+  { 
       // Your code here
-      vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
+      vector<vector<int>>t(n+1,vector<int>(W+1,-1));
       
-      return knap(W,wt,val,n,dp);
+      return knap(W,wt,val,n,t);
       
-   }
+  }
+
 };
 
 // { Driver Code Starts.
