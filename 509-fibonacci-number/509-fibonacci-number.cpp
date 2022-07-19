d1ecc4d3e3e495d4845611207e4517393f0a7ed7
Mayank <90982737+Mayanklakhina@users.noreply.github.com>
2022-07-19 13:21:37 +0530

    Time: 4 ms (49.68%), Space: 5.9 MB (39.74%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
  int fib(int n) {
-	if(n <= 1) // base condition
-		return n;
-	return fib(n - 1) + fib(n - 2);
+     if(n==0){
+         return 0;
+        }
+     if(n==1){
+        return 1;
+          }
+        int a=0,b=1,c=0;
+            
+            for(int i=1;i<n;i++){
+                c=a+b;
+                a=b;
+                b=c;
+            }
+            return c;
+   
+     
+	// if(n <= 1) // base condition
+	// 	return n;
+	// return fib(n - 1) + fib(n - 2);
 }
 };
