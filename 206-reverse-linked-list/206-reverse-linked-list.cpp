f0e3809fead74868929c92caee27af3e61d4b75b
Mayank <90982737+Mayanklakhina@users.noreply.github.com>
2022-08-07 16:46:08 +0530

    Time: 15 ms (18.94%), Space: 8.4 MB (42.11%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,16 +11,28 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        vector<int> answer;
-            
-            for(ListNode *curr=head;curr!=NULL;curr=curr->next){
-                    answer.push_back(curr->val);
+            if(head==NULL) return head;
+            ListNode *p1=NULL,*p2=head,*p3=head->next;
+            while(p2!=NULL){
+                    p2->next = p1;
+                    p1=p2;
+                    p2=p3;
+                    if(p3!=NULL)
+                            p3=p3->next;
             }
+            return p1;
             
-            for(ListNode *curr=head;curr!=NULL;curr=curr->next){
-                    curr->val = answer.back();
-                    answer.pop_back();
-            }
-            return head;
+//             Brute Force Approach
+//         vector<int> answer;
+            
+//             for(ListNode *curr=head;curr!=NULL;curr=curr->next){
+//                     answer.push_back(curr->val);
+//             }
+            
+//             for(ListNode *curr=head;curr!=NULL;curr=curr->next){
+//                     curr->val = answer.back();
+//                     answer.pop_back();
+//             }
+//             return head;
     }
 };
