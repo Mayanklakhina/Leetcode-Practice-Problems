875f2cf7647a1aed6c93f4ff298373c2942c7b0d
Mayank <90982737+Mayanklakhina@users.noreply.github.com>
2022-08-09 21:32:12 +0530

    Time: 373 ms (50.31%), Space: 114.1 MB (85.80%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -14,18 +14,25 @@ public:
             
             if(head==NULL || head->next==NULL)
               return true;
-            ListNode *temp = new ListNode(-1);
-            ListNode *ptr = temp;
-            ListNode *headptr = head;
-            int size = 0;
-            while(headptr!=NULL){
-                    size++;
-                    ListNode *x = new ListNode(headptr->val);
-                    ptr->next=x;
-                    ptr=ptr->next;
-                    headptr = headptr->next;
+            // ListNode *temp = new ListNode(-1);
+            // ListNode *ptr = temp;
+            // ListNode *headptr = head;
+            // int size = 0;
+            // while(headptr!=NULL){
+            //         size++;
+            //         ListNode *x = new ListNode(headptr->val);
+            //         ptr->next=x;
+            //         ptr=ptr->next;
+            //         headptr = headptr->next;
+            // }
+            ListNode *slow = head;
+            ListNode *fast = head;
+            
+            while(fast->next!=NULL && fast->next->next!=NULL){
+                    fast=fast->next->next;
+                    slow=slow->next;
             }
-            ListNode *p1=NULL,*p2=temp->next,*p3=temp->next->next;
+            ListNode *p1=NULL,*p2=slow->next,*p3=slow->next->next;
             while(p2!=NULL){
                     p2->next = p1;
                     p1=p2;
@@ -35,10 +42,9 @@ public:
             }
             
             ListNode *ptr1 = p1;
-           int count=0;
             
-            while(head!=NULL && ptr1!=NULL&&count<size/2){
-                    count++;
+            while(head!=NULL && ptr1!=NULL){
+                    
                   
             if(head->val !=ptr1->val) return false;
                    head=head->next;
