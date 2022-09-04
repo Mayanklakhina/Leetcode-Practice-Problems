af0bbc0021e30047e29742dc3a6ec9a2444e9882
Mayank <90982737+Mayanklakhina@users.noreply.github.com>
2022-09-04 23:05:33 +0530

    Time: 15 ms (93.33%), Space: 13.5 MB (97.51%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int> result;
-priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>> > minHeap;
-            unordered_map<int,int> map;
+        //create an unordered map
+        unordered_map <int,int> map;
+            //declaring a resulting vector
+            vector<int> result;
+        
+            //create a minimum heap
+    priority_queue<pair<int,int> , vector<pair<int,int>>,greater<pair<int,int>> > pq;
+         for ( int i = 0; i<nums.size();i++){
+                 //storing the frequencies
+                 map[nums[i]]++;
+         }
             
-            for(int i=0;i<nums.size();i++){
-             //for storing the values with their respective frequency
-                    map[nums[i]]++;
-            }
-            
-            for(auto i=map.begin();i!=map.end();i++){
-                    minHeap.push({i->second,i->first});
-                    
-                    if(minHeap.size()>k)
-                            minHeap.pop();
-            }
-            while(minHeap.size()>0){
-                    result.push_back(minHeap.top().second);
-                    minHeap.pop();
+         for(auto i=map.begin();i!=map.end();i++){
+                 pq.push({i->second,i->first});
+                 
+                 if(pq.size() > k)
+                 pq.pop();
+         }
+            while(!pq.empty()){
+                    result.push_back(pq.top().second);
+                    pq.pop();
             }
             return result;
     }
